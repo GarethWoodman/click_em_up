90a4fedf1e79dfb0cc80a0c8b405da546bde206a
GarethWoodman <gareth.woodman92@gmail.com>
2020-07-24 21:49:18 +0100

    Added js functionality for 'boss' to take damage

--- src/Interface.js
+++ src/Interface.js
@@ -1,2 +1,13 @@
 $(document).ready(function() {
+  var boss = new Boss();
+  boss.hp = 100;
+
+  $('#boss').on('click', function() {
+    boss.takeDamage(5)
+    update()
+  });
+
+  function update() {
+    $('#boss_hp_bar').text(boss.hp)
+  }
 });
