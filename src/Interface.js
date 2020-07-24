42eea71aa1c716a84e262c88035c8ec3dc5e7b92
GarethWoodman <gareth.woodman92@gmail.com>
2020-07-24 22:14:58 +0100

    Added player to interface

--- src/Interface.js
+++ src/Interface.js
@@ -1,15 +1,29 @@
-$(document).ready(function() {
-  var boss = new Boss();
-  boss.hp = 100;
+var boss = new Boss();
+var player = new Player();
 
+function configureBoss(){
+  boss.hp = 100;
+}
+
+function configurePlayer(){
+  player.atkPower = 1;
+}
+
+function configureModels(){
+  configureBoss();
+  configurePlayer();
+}
+
+function update() {
+  $('#boss_hp_bar').text(boss.hp)
+}
+
+$(document).ready(function() {
+  configureModels();
   update()
 
   $('#boss').on('click', function() {
-    boss.takeDamage(5)
+    boss.takeDamage(player.atkPower)
     update()
   });
-
-  function update() {
-    $('#boss_hp_bar').text(boss.hp)
-  }
 });
