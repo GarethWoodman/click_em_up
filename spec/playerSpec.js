444ea01058779e6d83c4073c03b9ed82eb57e1d7
GarethWoodman <gareth.woodman92@gmail.com>
2020-07-24 21:59:37 +0100

    Tested atkPower attribute

--- spec/playerSpec.js
+++ spec/playerSpec.js
@@ -1,3 +1,14 @@
 describe("Player", function() {
   let player = new Player();
+  const damage = 1;
+
+  beforeEach(function() {
+    player.atkPower = damage
+  })
+
+  describe("check atkPower", function() {
+    it("should return damage", function() {
+      expect(player.atkPower).toEqual(damage)
+    })
+  })
 })
