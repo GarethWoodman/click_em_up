4643549574db3868fd3b765e15013de6b129a861
GarethWoodman <gareth.woodman92@gmail.com>
2020-07-24 22:35:41 +0100

    Added testing for player hp

--- spec/playerSpec.js
+++ spec/playerSpec.js
@@ -1,14 +1,23 @@
 describe("Player", function() {
   let player = new Player();
+
   const damage = 1;
+  const maxHP  = 100;
 
   beforeEach(function() {
     player.atkPower = damage
+    player.hp = maxHP
   })
 
   describe("check atkPower", function() {
     it("should return damage", function() {
       expect(player.atkPower).toEqual(damage)
+    })
+  })
+
+  describe("check HP", function() {
+    it("should return HP", function() {
+      expect(player.hp).toEqual(maxHP)
     })
   })
 })
